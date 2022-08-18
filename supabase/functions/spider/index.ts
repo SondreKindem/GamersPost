cd665233e2e6ef8a9a03ebd43764287f39512446
Sondre Kindem <sondre.kindem@gmail.com>
2022-08-18 19:21:57 +0200

    Fix duplicate articles being saved

--- supabase/functions/spider/index.ts
+++ supabase/functions/spider/index.ts
@@ -16,61 +16,50 @@ const corsHeaders = {
 
 serve(async (req) => {
     try {
-        const supabase = createClient(
-            Deno.env.get('SUPABASE_URL') ?? '',
-            Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
-        )
-
-        const now = new Date()
-        now.setDate(now.getDate() - 15)
-        const {data: existingArticles, error} = await supabase
-            .from('articles')
-            .select('link,published')
-            .gt("published", now.toISOString())
-
-        if (!existingArticles && error) {
-            console.log(JSON.stringify(error))
-            return
-        }
-
         // loop through new articles, and check if they exist in old
         const newArticles: DbArticle[] = [].concat(
-            await fetchAndParseFeed("https://techraptor.net/feed", 2, existingArticles),
-            await fetchAndParseFeed("https://www.eurogamer.net/feed", 3, existingArticles),
-            await fetchAndParseFeed("https://kotaku.com/rss", 4, existingArticles),
-            await fetchAndParseFeed("https://www.destructoid.com/feed", 5, existingArticles),
-            await fetchAndParseFeed("https://www.vg247.com/feed/", 6, existingArticles),
-            await fetchAndParseFeed("https://www.gamespot.com/feeds/mashup", 7, existingArticles),
-            await fetchAndParseFeed("https://feeds.feedburner.com/ign/news", 8, existingArticles),
-            await fetchAndParseFeed("https://www.polygon.com/rss/index.xml", 9, existingArticles),
-            await fetchAndParseFeed("https://www.rockpapershotgun.com/feed", 10, existingArticles),
-            await fetchAndParseFeed("https://www.gameinformer.com/feeds/thefeedrss.aspx", 11, existingArticles),
-            await fetchAndParseFeed("https://www.pcgamer.com/rss/", 1, existingArticles),
-            await fetchAndParseFeed("https://www.engadget.com/rss.xml", 12, existingArticles),
-            await fetchAndParseFeed("https://www.pcgamesn.com/mainrss.xml", 13, existingArticles), // TODO: Duplicate articles
-            await fetchAndParseFeed("https://www.dualshockers.com/feed/rss", 14, existingArticles), // TODO: No images
-            await fetchAndParseFeed("https://nichegamer.com/feed/", 15, existingArticles),
-            await fetchAndParseFeed("https://www.siliconera.com/feed/", 16, existingArticles),
-            await fetchAndParseFeed("https://www.gamesindustry.biz/feed", 17, existingArticles),
-            await fetchAndParseFeed("https://stevivor.com/feed", 18, existingArticles),
-            await fetchAndParseFeed("https://gamerant.com/feed/", 19, existingArticles),
+            await fetchAndParseFeed("https://techraptor.net/feed", 2),
+            await fetchAndParseFeed("https://www.eurogamer.net/feed", 3),
+            await fetchAndParseFeed("https://kotaku.com/rss", 4),
+            await fetchAndParseFeed("https://www.destructoid.com/feed", 5),
+            await fetchAndParseFeed("https://www.vg247.com/feed/", 6),
+            await fetchAndParseFeed("https://www.gamespot.com/feeds/mashup", 7),
+            await fetchAndParseFeed("https://feeds.feedburner.com/ign/news", 8),
+            await fetchAndParseFeed("https://www.polygon.com/rss/index.xml", 9),
+            await fetchAndParseFeed("https://www.rockpapershotgun.com/feed", 10),
+            await fetchAndParseFeed("https://www.gameinformer.com/feeds/thefeedrss.aspx", 11),
+            await fetchAndParseFeed("https://www.pcgamer.com/rss/", 1),
+            await fetchAndParseFeed("https://www.engadget.com/rss.xml", 12),
+            await fetchAndParseFeed("https://www.pcgamesn.com/mainrss.xml", 13), // TODO: Duplicate articles
+            await fetchAndParseFeed("https://www.dualshockers.com/feed/rss", 14), // TODO: No images
+            await fetchAndParseFeed("https://nichegamer.com/feed/", 15),
+            await fetchAndParseFeed("https://www.siliconera.com/feed/", 16),
+            await fetchAndParseFeed("https://www.gamesindustry.biz/feed", 17),
+            await fetchAndParseFeed("https://stevivor.com/feed", 18),
+            await fetchAndParseFeed("https://gamerant.com/feed/", 19),
         )
 
         console.log("Found new articles: " + newArticles.length)
 
-        const {data: insertData, error: insertError} = await supabase
-            .from('articles')
-            .insert(newArticles)
+        if(newArticles.length > 0) {
+            const supabase = createClient(
+                Deno.env.get('SUPABASE_URL') ?? '',
+                Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+            )
+            const {data: insertData, error: insertError} = await supabase
+                .from('articles')
+                .insert(newArticles)
 
-        if (insertError != null) {
-            const responseString = JSON.stringify({
-                msg: "Error inserting to db",
-                error: insertError
-            })
-            return new Response(responseString, {
-                headers: {"Content-Type": "application/json"},
-                status: 400
-            })
+            if (insertError != null) {
+                const responseString = JSON.stringify({
+                    msg: "Error inserting to db",
+                    error: insertError
+                })
+                return new Response(responseString, {
+                    headers: {"Content-Type": "application/json"},
+                    status: 400
+                })
+            }
         }
 
         return new Response("OK", {
@@ -88,10 +77,31 @@ serve(async (req) => {
     }
 })
 
+async function fetchExistingArticles(siteId: number, fromDate: Date) {
+    const supabase = createClient(
+        Deno.env.get('SUPABASE_URL') ?? '',
+        Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+    )
+
+    const {data: existingArticles, error} = await supabase
+        .from('articles')
+        .select('link,published')
+        .eq('website_id', siteId)
+        .gt("published", fromDate.toISOString())
+
+    if (!existingArticles && error) {
+        const errText = JSON.stringify(error)
+        console.log(errText)
+        throw errText
+    }
+
+    return existingArticles
+}
+
 async function fetchAndParseFeed(url: string, siteId: number, existingArticles: DbArticle[]) {
     try {
         const rawFeed = await fetchFeed(url)
-        return getNewArticles(rawFeed, siteId, existingArticles)
+        return await getNewArticles(rawFeed, siteId, existingArticles)
     } catch (e) {
         console.log("Failed to fetch for site: " + siteId)
         console.log(e.message)
@@ -100,12 +110,14 @@ async function fetchAndParseFeed(url: string, siteId: number, existingArticles: 
     }
 }
 
-function getNewArticles(rssFeed: RssModel, siteId: number, existingArticles: DbArticle[]): DbArticle[] {
+async function getNewArticles(rssFeed: RssModel, siteId: number): Promise<DbArticle[]> {
     const newArticles: DbArticle[] = []
     console.log("PARSING SITE " + siteId)
     const now = new Date()
-    now.setDate(now.getDate() - 15)
+    now.setDate(now.getDate() - 7)
     const timeNow = now.getTime()
+
+    const existingArticles: DbArticle[] = await fetchExistingArticles(siteId, now)
 
     for (const article of rssFeed.items) {
         if (existingArticles.some(a => a.link === article.links[0].url && new Date(a.published).getTime() === new Date(article.published).getTime())) {
